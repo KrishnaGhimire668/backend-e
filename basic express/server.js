599d00bd3c4ddd0c9ff0a8825ed8f97bc1e128fe
KrishnaGhimire668 <136567440+KrishnaGhimire668@users.noreply.github.com>
2025-12-26 23:05:45 +0545

    added json

--- basic express/server.js
+++ basic express/server.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
 
 app.use(function(req, res, next){
-    console.log('middleware chala')
+    console.log('middleware start')
     next()
 })
 
